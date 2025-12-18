8c4de4bf002641b126f80088a5f4158411f11e08
Griffin <33357138+DGriffin91@users.noreply.github.com>
2025-12-18 14:36:03 -0800

    make shader compilation a fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use glow::HasContext;
+
 #[cfg(not(target_arch = "wasm32"))]
 pub type GlProgram = glow::NativeProgram;
 #[cfg(target_arch = "wasm32")]
@@ -103,7 +105,6 @@ impl BevyGlContext {
         }
         #[cfg(target_arch = "wasm32")]
         {
-            use glow::HasContext;
             use wasm_bindgen::JsCast;
             let canvas = winit_window.canvas().unwrap();
 
@@ -122,6 +123,56 @@ impl BevyGlContext {
             let gl = glow::Context::from_webgl1_context(webgl_context);
             unsafe { gl.viewport(0, 0, width as i32, height as i32) };
             BevyGlContext { gl }
+        }
+    }
+
+    pub fn shader(&self, vertex: &str, fragment: &str) -> GlProgram {
+        unsafe {
+            let program = self.gl.create_program().expect("Cannot create program");
+
+            let shader_sources = [
+                (glow::VERTEX_SHADER, vertex),
+                (glow::FRAGMENT_SHADER, fragment),
+            ];
+
+            let mut shaders = Vec::with_capacity(shader_sources.len());
+
+            for (shader_type, shader_source) in shader_sources.iter() {
+                let shader = self
+                    .gl
+                    .create_shader(*shader_type)
+                    .expect("Cannot create shader");
+
+                #[cfg(target_arch = "wasm32")]
+                let preamble = "precision highp float;";
+                #[cfg(not(target_arch = "wasm32"))]
+                let preamble = "#version 120";
+
+                self.gl
+                    .shader_source(shader, &format!("{}\n{}", preamble, shader_source));
+
+                self.gl.compile_shader(shader);
+
+                if !self.gl.get_shader_compile_status(shader) {
+                    panic!("{}", self.gl.get_shader_info_log(shader));
+                }
+
+                self.gl.attach_shader(program, shader);
+                shaders.push(shader);
+            }
+
+            self.gl.link_program(program);
+
+            if !self.gl.get_program_link_status(program) {
+                panic!("{}", self.gl.get_program_info_log(program));
+            }
+
+            for shader in shaders {
+                self.gl.detach_shader(program, shader);
+                self.gl.delete_shader(shader);
+            }
+
+            program
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -42,70 +42,27 @@ fn init(world: &mut World, params: &mut SystemState<Query<(Entity, &mut Window)>
 }
 
 fn triangle(world: &mut World) {
-    let mut ctx = world.non_send_resource_mut::<BevyGlContext>();
-    let gl = &mut ctx.gl;
+    let ctx = world.non_send_resource_mut::<BevyGlContext>();
 
-    let program = unsafe { gl.create_program().expect("Cannot create program") };
-
-    let vertex_shader_source = r#"
+    let vertex = r#"
 attribute vec2 a_position;
 varying vec2 vert;
 
 void main() {
-    vert = a_position;
-    gl_Position = vec4(a_position - vec2(0.5, 0.5), 0.0, 1.0);
-}
-        "#;
-
-    let fragment_shader_source = r#"
-
-varying vec2 vert;
-
-void main() {
-    gl_FragColor = vec4(vert, 0.0, 1.0);
+vert = a_position;
+gl_Position = vec4(a_position - vec2(0.5, 0.5), 0.0, 1.0);
 }
     "#;
 
-    let shader_sources = [
-        (glow::VERTEX_SHADER, vertex_shader_source),
-        (glow::FRAGMENT_SHADER, fragment_shader_source),
-    ];
+    let fragment = r#"
+varying vec2 vert;
 
-    let mut shaders = Vec::with_capacity(shader_sources.len());
+void main() {
+gl_FragColor = vec4(vert, 0.0, 1.0);
+}
+"#;
 
-    for (shader_type, shader_source) in shader_sources.iter() {
-        let shader = unsafe {
-            gl.create_shader(*shader_type)
-                .expect("Cannot create shader")
-        };
-
-        #[cfg(target_arch = "wasm32")]
-        let preamble = "precision highp float;";
-        #[cfg(not(target_arch = "wasm32"))]
-        let preamble = "#version 120";
-
-        unsafe { gl.shader_source(shader, &format!("{}\n{}", preamble, shader_source)) };
-        unsafe { gl.compile_shader(shader) };
-        unsafe {
-            if !gl.get_shader_compile_status(shader) {
-                panic!("{}", gl.get_shader_info_log(shader));
-            }
-        }
-        unsafe { gl.attach_shader(program, shader) };
-        shaders.push(shader);
-    }
-
-    unsafe { gl.link_program(program) };
-    unsafe {
-        if !gl.get_program_link_status(program) {
-            panic!("{}", gl.get_program_info_log(program));
-        }
-    }
-
-    for shader in shaders {
-        unsafe { gl.detach_shader(program, shader) };
-        unsafe { gl.delete_shader(shader) };
-    }
+    let program = ctx.shader(vertex, fragment);
 
     world.insert_non_send_resource(TriangleProgram { program });
 }
